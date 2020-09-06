fda37bd99d83e786c6df6f5905ba7b29f18bb0a6
alpoliakov <alpoliakov73@gmail.com>
2020-09-06 20:13:35 +0300

    feat: Created starter dependencies

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    REACT_APP_APIKEY: 'AIzaSyCh-V3EQzKjcMYf7MX7P8-LGBSyIrhz78I',
+    REACT_APP_AUTHDOMAIN: 'x-check.firebaseapp.com',
+    REACT_APP_DB: 'https://x-check.firebaseio.com',
+    REACT_APP_PID: 'x-check',
+    REACT_APP_SB: 'x-check.appspot.com',
+    REACT_APP_SID: '266993605155',
+    REACT_APP_APPID: '1:266993605155:web:d5490f2b54dfb662dbef96',
+    REACT_APP_MID: 'G-TKKVGZYFC7',
+  },
+};
