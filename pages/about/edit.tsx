a61194766b50653012e98fe25a44d32e1359e4c5
alpoliakov <alpoliakov73@gmail.com>
2020-09-09 22:44:34 +0300

    feat: Create edit page

---
+++ pages/about/edit.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import MainLayout from '../../components/MainLayout';
+
+const EditUser = () => {
+  return (
+    <>
+      <MainLayout title={'Edit'}>
+        <h1>Edit User Page</h1>
+      </MainLayout>
+    </>
+  );
+};
+
+export default EditUser;
