592254b99670a94a9e266527183fea2d68771f0a
alpoliakov <alpoliakov73@gmail.com>
2020-09-10 02:42:51 +0300

    feat: Added edit structure & changed user structure

--- pages/about/edit.tsx
+++ pages/about/edit.tsx
@@ -1,11 +1,119 @@
 import React, { useState, useEffect } from 'react';
 import MainLayout from '../../components/MainLayout';
+import { Button, Card, Col, Row, Avatar, Typography, Empty } from 'antd';
+import {
+  GithubOutlined,
+  EnvironmentOutlined,
+  EditOutlined,
+  SettingOutlined,
+  InfoCircleOutlined,
+  TagOutlined,
+  ReadOutlined,
+  ContactsOutlined,
+  NotificationOutlined,
+  BookOutlined,
+} from '@ant-design/icons';
 
 const EditUser = () => {
+  const { Title, Link, Text } = Typography;
   return (
     <>
       <MainLayout title={'Edit'}>
-        <h1>Edit User Page</h1>
+        <div className="site-card-wrapper">
+          <Row gutter={32}>
+            <Col span={6}>
+              <Card
+                bordered={true}
+                className="intro"
+                actions={[<EditOutlined key="edit" />, <SettingOutlined key="setting" />]}
+              >
+                <Avatar size={90} src="/static/images/king.jpg" />
+                <Title level={2}>Oleksandr Poliakov</Title>
+                <Title level={5}>
+                  <Link href="https://github.com/">
+                    <GithubOutlined /> githab
+                  </Link>
+                </Title>
+                <div>
+                  <Text>
+                    <EnvironmentOutlined /> Location
+                  </Text>
+                </div>
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<InfoCircleOutlined key="info" />, <span> About</span>]}
+                bordered={true}
+                actions={[<EditOutlined key="edit" />, <SettingOutlined key="setting" />]}
+              >
+                <Empty
+                  description="About info isn't written"
+                  image={Empty.PRESENTED_IMAGE_SIMPLE}
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<TagOutlined key="english" />, <span> Estimated English level</span>]}
+                bordered={true}
+                actions={[<EditOutlined key="edit" />, <SettingOutlined key="setting" />]}
+              >
+                <Empty
+                  description="English level isn't choosen"
+                  image={Empty.PRESENTED_IMAGE_SIMPLE}
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<ReadOutlined key="english" />, <span> Education</span>]}
+                bordered={true}
+                actions={[<EditOutlined key="edit" />, <SettingOutlined key="setting" />]}
+              >
+                <Empty
+                  description="Education history isn't filled in"
+                  image={Empty.PRESENTED_IMAGE_SIMPLE}
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<ContactsOutlined key="contacts" />, <span> Contacts</span>]}
+                bordered={true}
+                actions={[<EditOutlined key="edit" />, <SettingOutlined key="setting" />]}
+              >
+                <Empty
+                  description="Contacts aren't filled in"
+                  image={Empty.PRESENTED_IMAGE_SIMPLE}
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<NotificationOutlined key="consents" />, <span> Consents</span>]}
+                bordered={true}
+                actions={[<EditOutlined key="edit" />]}
+              >
+                <Empty description="Consents" image={Empty.PRESENTED_IMAGE_SIMPLE} />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<BookOutlined key="english" />, <span> Student Statistics</span>]}
+                bordered={true}
+                actions={[<SettingOutlined key="setting" />]}
+              >
+                <Empty description="Statistics" image={Empty.PRESENTED_IMAGE_SIMPLE} />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card bordered={true} bodyStyle={{ textAlign: 'center' }}>
+                <Link>Return to pages</Link>
+              </Card>
+            </Col>
+          </Row>
+        </div>
       </MainLayout>
     </>
   );
--- pages/about/user.tsx
+++ pages/about/user.tsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import MainLayout from '../../components/MainLayout';
 import { checkRef, auth } from '../../firebase';
-import { Card, Avatar, Divider, List } from 'antd';
 import { LogoutOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
-
-const { Meta } = Card;
+import { Button, Card, Col, Row, Avatar, Typography, Empty } from 'antd';
+import {
+  GithubOutlined,
+  EnvironmentOutlined,
+  EditOutlined,
+  SettingOutlined,
+  InfoCircleOutlined,
+  TagOutlined,
+  ReadOutlined,
+  ContactsOutlined,
+  NotificationOutlined,
+  BookOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
 
 interface PropsUser {
   title: string;
@@ -18,6 +29,7 @@ const User: React.FC<PropsUser> = ({ title }) => {
   const [uid, setUid] = useState('');
   const [roles, setRoles] = useState([]);
   const router = useRouter();
+  const { Title, Link, Text } = Typography;
 
   useEffect(() => {
     checkRef.on('value', (snapshot) => {
@@ -41,41 +53,97 @@ const User: React.FC<PropsUser> = ({ title }) => {
 
   return (
     <>
-      <MainLayout title={`User page: ${name}`}>
-        <main className={'main__about'}>
-          <div>
-            <Card
-              style={{ width: 420 }}
-              cover={
-                <img
-                  id="login-card-img"
-                  className="login-card-img"
-                  alt="logo RS School"
-                  src="/static/images/logo-rs-school.svg"
+      <MainLayout title={'User'}>
+        <div className="site-card-wrapper">
+          <Row gutter={16}>
+            <Col span={6}>
+              <Card bordered={true} className="intro">
+                <Avatar size={90} src="/static/images/king.jpg" />
+                <Title level={2}>{name}</Title>
+                <Title level={5}>
+                  <Link href="https://github.com/">
+                    <GithubOutlined /> githab
+                  </Link>
+                </Title>
+                <div>
+                  <Text>
+                    <EnvironmentOutlined /> location
+                  </Text>
+                </div>
+                <div>
+                  <Text>
+                    <MailOutlined /> {email}
+                  </Text>
+                </div>
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<InfoCircleOutlined key="info" />, <span> About</span>]}
+                bordered={true}
+              >
+                <Empty
+                  description="About info isn't written"
+                  image={Empty.PRESENTED_IMAGE_SIMPLE}
                 />
-              }
-              actions={[<LogoutOutlined key="logout" onClick={returnToPage} />]}
-            >
-              <Meta
-                avatar={
-                  <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                }
-                title="YOUR PROFILE"
-                description="Information about you"
-              />
-              <List size="small">
-                <Divider orientation="center">Nickname</Divider>
-                <h4>{name}</h4>
-                <Divider orientation="center">Email</Divider>
-                <h4>{email}</h4>
-                <Divider orientation="center">ID</Divider>
-                <h4>{uid}</h4>
-                <Divider orientation="center">Roles</Divider>
-                <h4>{roles.join(', ')}</h4>
-              </List>
-            </Card>
-          </div>
-        </main>
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<TagOutlined key="english" />, <span> Estimated English level</span>]}
+                bordered={true}
+              >
+                <Empty
+                  description="English level isn't choosen"
+                  image={Empty.PRESENTED_IMAGE_SIMPLE}
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<ReadOutlined key="english" />, <span> Education</span>]}
+                bordered={true}
+              >
+                <Empty
+                  description="Education history isn't filled in"
+                  image={Empty.PRESENTED_IMAGE_SIMPLE}
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<ContactsOutlined key="contacts" />, <span> Contacts</span>]}
+                bordered={true}
+              >
+                <Empty
+                  description="Contacts aren't filled in"
+                  image={Empty.PRESENTED_IMAGE_SIMPLE}
+                />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<NotificationOutlined key="consents" />, <span> Consents</span>]}
+                bordered={true}
+              >
+                <Empty description="Consents" image={Empty.PRESENTED_IMAGE_SIMPLE} />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card
+                title={[<BookOutlined key="english" />, <span> Student Statistics</span>]}
+                bordered={true}
+              >
+                <Empty description="Statistics" image={Empty.PRESENTED_IMAGE_SIMPLE} />
+              </Card>
+            </Col>
+            <Col span={6}>
+              <Card bordered={true} bodyStyle={{ textAlign: 'center' }}>
+                <Link onClick={returnToPage}>Return to pages</Link>
+              </Card>
+            </Col>
+          </Row>
+        </div>
       </MainLayout>
     </>
   );
