6bb018ceb4b4e87ec7281e3f7c6cc2033c722d44
igorzima <redteam122@gmail.com>
2020-09-30 17:14:44 +0300

    feat: add dataReviews

--- pages/main/admin/index.tsx
+++ pages/main/admin/index.tsx
@@ -11,7 +11,7 @@ interface PropsAdmin {
   dataUsers: [];
   dataTasks: [];
   crossCheckSession: [];
-  dataRow?: [];
+  dataReviews: [];
   dataSession: [];
 }
 
@@ -19,7 +19,7 @@ const AdminPage: React.FC<PropsAdmin> = ({
   dataUsers,
   dataTasks,
   crossCheckSession,
-  dataRow,
+  dataReviews,
   dataSession,
 }) => {
   const { Title } = Typography;
@@ -47,6 +47,7 @@ const AdminPage: React.FC<PropsAdmin> = ({
     setVisitableTable(false);
   };
   const task = {};
+
   return (
     <MainLayout title={'main: admin'}>
       <Title level={1}>Admin Page</Title>
@@ -101,7 +102,7 @@ const AdminPage: React.FC<PropsAdmin> = ({
             visible={visitableTable}
             onOk={handleOkTable}
           >
-            <TableData dataRow={dataRow} />
+            <TableData dataRow={dataReviews} />
           </Modal>
         </div>
       </main>
@@ -113,9 +114,8 @@ export const getServerSideProps = async () => {
   let dataUsers: any | undefined = [];
   let dataTasks: any | undefined = [];
   let crossCheckSession: any | undefined = [];
-  // let courseUser: any | undefined = [];
-  // let courseCrossCheckTasks: any | undefined = [];
   let dataSession: any | undefined = [];
+  let completedTask: any | undefined = [];
   await db
     .collection('sessions')
     .get()
@@ -128,7 +128,6 @@ export const getServerSideProps = async () => {
     .get()
     .then((snap) => {
       dataUsers = snap.docs.map((doc) => doc.data());
-      // courseUser = data.filter((user) => user.course.includes(activeCourse));
     });
   await db
     .collection('TasksArray')
@@ -142,236 +141,34 @@ export const getServerSideProps = async () => {
     .then((snap) => {
       crossCheckSession = snap.docs.map((doc) => doc.data());
     });
-
-  /*   await db
-    .collection('course')
+  await db
+    .collection('completed_tasks')
     .get()
     .then((snap) => {
-      courseCrossCheckTasks = snap.docs
-        .map((doc) => doc.data())
-        .filter((el) => el.name === activeCourse)[0]
-        .map((task) => task.checkingType === 'crossCheck');
-    }); */
+      completedTask = snap.docs.map((doc) => doc.data());
+    });
 
-  const courseUser = [
-    {
-      location: 'unknown',
-      login: 'mariariazanova',
-      avatar_url: 'https://avatars3.githubusercontent.com/u/57406080?v=4',
-      html_url: 'https://github.com/mariariazanova',
-      nickname: 'mariariazanova',
-      email: 'mv_13@mail.ru',
-      password: 'ZZzz1122',
-      uid: '41iPtdzIYHV5XxwnXRgOm1Nr14H3',
-      roles: ['student', 'mentor', 'admin', 'manager'],
-      name: 'Maria Riazanova',
-      task: {
-        scores: [
-          { reviewer: 'egor', score: '0' },
-          { reviewer: 'stas', score: '12' },
-        ],
-        id: 'utuygj',
-      },
-      task1: {
-        scores: [
-          { reviewer: 'egor', score: '30' },
-          { reviewer: 'stas', score: '50' },
-        ],
-        id: 'eryrytr',
-      },
-    },
-    {
-      html_url: 'https://github.com/alpoliakov',
-      nickname: 'alpoliakov',
-      roles: ['student', 'admin', 'mentor', 'manager'],
-      location: 'Kyiv',
-      avatar_url: 'https://avatars1.githubusercontent.com/u/27024108?v=4',
-      name: 'Aleksandr Poliakov',
-      login: 'alpoliakov',
-      email: 'alpoliakov73@gmail.com',
-      uid: '5iwVTjafzVayLNusWyaxqbaTB6u1',
-      task: {
-        scores: [
-          { reviewer: 'egor', score: '15' },
-          { reviewer: 'stas', score: '1' },
-        ],
-        id: 'qwqeq',
-      },
-      task1: {
-        scores: [
-          { reviewer: 'egor', score: '30' },
-          { reviewer: 'stas', score: '50' },
-        ],
-        id: 'tertry',
-      },
-    },
-    {
-      html_url: 'https://github.com/cup0ra',
-      uid: 'JpeOZBnDFOdWFnny8B2to58DKNg1',
-      name: 'sergey',
-      login: 'cup0ra',
-      avatar_url: 'https://avatars1.githubusercontent.com/u/57291691?v=4',
-      roles: ['student', 'admin', 'mentor', 'manager'],
-      nickname: 'cup0ra',
-      email: null,
-      location: null,
-      task: {
-        scores: [
-          { reviewer: 'egor', score: '100' },
-          { reviewer: 'stas', score: '19' },
-        ],
-        id: 'jgugu',
-      },
-      task1: {
-        scores: [
-          { reviewer: 'egor', score: '30' },
-          { reviewer: 'stas', score: '50' },
-        ],
-        id: 'hrthtrh',
-      },
-    },
-    {
-      name: null,
-      email: null,
-      roles: ['student', 'admin', 'mentor', 'manager'],
-      nickname: 'SLatyankov',
-      uid: 'Q3npcG2fnxbOPrnunZNnbp92Zdz2',
-      avatar_url: 'https://avatars1.githubusercontent.com/u/47479375?v=4',
-      login: 'SLatyankov',
-      html_url: 'https://github.com/SLatyankov',
-      location: null,
-      task: {
-        scores: [
-          { reviewer: 'egor', score: '55' },
-          { reviewer: 'stas', score: '13' },
-        ],
-        id: 'adada',
-      },
-      task1: {
-        scores: [
-          { reviewer: 'egor', score: '30' },
-          { reviewer: 'stas', score: '50' },
-        ],
-        id: 'qrsdsgd',
-      },
-    },
-    {
-      nickname: 'igorzima',
-      location: null,
-      login: 'igorzima',
-      roles: ['student', 'admin', 'mentor', 'manager'],
-      avatar_url: 'https://avatars0.githubusercontent.com/u/49781540?v=4',
-      name: 'Ihar Zimnitski',
-      uid: 'sZvSHsJdnRQyaNkAERz9eaj0ra03',
-      email: null,
-      html_url: 'https://github.com/igorzima',
-      task: {
-        scores: [
-          { reviewer: 'egor', score: '35' },
-          { reviewer: 'stas', score: '40' },
-        ],
-        id: 'kuitiu',
-      },
-      task1: {
-        scores: [
-          { reviewer: 'egor', score: '30' },
-          { reviewer: 'stas', score: '50' },
-        ],
-        id: 'qrwrew',
-      },
-    },
-    {
-      roles: ['student', 'mentor', 'admin', 'manager'],
-      login: 'GameBoy',
-      email: 'alpoliakov@gmail.com',
-      uid: 'xfsmOXqeFjd8ZpbnocMt3Qh1y3K3',
-      html_url: 'https://github.com/GameBoy',
-      name: 'Game Boy',
-      password: '123456',
-      location: 'unknown',
-      nickname: 'Gameboy',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/38436537?v=4',
-      task: {
-        scores: [
-          { reviewer: 'egor', score: '30' },
-          { reviewer: 'stas', score: '50' },
-        ],
-        id: 'vbnvnb',
-      },
-      task1: {
-        scores: [
-          { reviewer: 'egor', score: '30' },
-          { reviewer: 'stas', score: '50' },
-        ],
-        id: 'fsfe',
-      },
-    },
-  ];
+  const dataReviews = completedTask
+    .map((task) => {
+      if (task.cheсks.length > 0) {
+        return task.cheсks.map((el) => {
+          const reviewerName = task.reviewers.filter((reviewer) => reviewer.id === el.checkerID)[0];
 
-  const courseCrossCheckTasks = [
-    {
-      id: 'task',
-      name: 'task',
-      checkingType: 'crossCheck',
-    },
-    {
-      id: 'task1',
-      name: 'task1',
-      checkingType: 'not crossCheck',
-    },
-    {
-      id: 'task2',
-      name: 'task2',
-      checkingType: 'crossCheck',
-    },
-    {
-      id: 'task3',
-      name: 'task3',
-      checkingType: 'not crossCheck',
-    },
-  ];
-
-  // const dataRow = courseCrossCheckTasks
-  //   .filter((task) => task.checkingType === 'crossCheck')
-  //   .map((taskName) =>
-  //     courseUser
-  //       .filter((user) => user[taskName.name])
-  //       .map((user, index) => ({
-  //         key: index,
-  //         user: user.nickname,
-  //         task: taskName.name,
-  //         reviewer: 'ant',
-  //         score: 0,
-  //       }))
-  //   )
-  //   .flat(Infinity);
-
-  const dataRow = [
-    {
-      key: '1',
-      user: 'Mike',
-      task: '32',
-      reviewer: 'Stas',
-      score: 50,
-    },
-    {
-      key: '2',
-      user: 'John',
-      task: 'Singolo',
-      reviewer: 'Gleb',
-      score: 40,
-    },
-    {
-      key: '3',
-      user: 'Anna',
-      task: 'X-check',
-      reviewer: 'Gleb',
-      score: 30,
-    },
-  ];
+          return {
+            key: task.student.name,
+            user: task.student.name,
+            task: task.taskID,
+            reviewer: reviewerName,
+            score: el.score,
+          };
+        });
+      }
+    })
+    .filter((el) => el)
+    .flat(Infinity);
 
   return {
-    props: { dataUsers, dataTasks, crossCheckSession, dataRow, dataSession },
+    props: { dataUsers, dataTasks, crossCheckSession, dataReviews, dataSession },
   };
 };
 export default AdminPage;
