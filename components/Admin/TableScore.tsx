088433c3fad9560643a99a69541cac097f07e712
cup0ra <cup0ra@users.noreply.github.com>
2020-10-03 17:30:38 +0300

    fix: table

--- components/Admin/TableScore.tsx
+++ components/Admin/TableScore.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Space, Table } from 'antd';
+import { title } from 'process';
 import React, { useState } from 'react';
 import { ICourse } from '../../interfaces/ICourse';
 import { ITask } from '../../interfaces/ITask';
@@ -11,9 +12,23 @@ interface PropsScore {
   dataCompletedTask: IWorkDone[];
 }
 const TableScore: React.FC<PropsScore> = ({ dataUsers, dataTasks, dataCompletedTask }) => {
+  const columns: any = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      fixed: 'left',
+    },
+  ];
+  let obj: any = {};
+  for (let i = 0; i < dataTasks.length; i++) {
+    columns.push({ title: dataTasks[i].id, dataIndex: dataTasks[i].id, key: i });
+    obj[dataTasks[i].id] = 0;
+  }
   const datas = [];
   for (let i = 0; i < dataUsers.length; i++) {
-    const obj = {
+    obj = {
+      ...obj,
       key: dataUsers[i].uid,
       name: (
         <Space>
@@ -28,27 +43,21 @@ const TableScore: React.FC<PropsScore> = ({ dataUsers, dataTasks, dataCompletedT
           {dataUsers[i].nickname}
         </Space>
       ),
-
-      score: 0,
     };
+    for (const keys in obj) {
+      for (let i = 0; i < dataCompletedTask.length; i++) {
+        if (keys === dataCompletedTask[i].taskID && obj.key === dataCompletedTask[i].student.id) {
+          console.log('111111', obj, (obj[keys] = dataCompletedTask[i].finalScore));
+          obj[dataCompletedTask[i].taskID] = dataCompletedTask[i].finalScore;
+        }
+      }
+    }
+
     datas.push(obj);
   }
-  for (let i = 0; i < datas.length; i++) {}
-  const task = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      fixed: 'left',
-    },
-  ];
-  for (let i = 0; i < dataTasks.length; i++) {
-    task.push({ title: dataTasks[i].id, dataIndex: 'score', key: i });
-  }
-  console.log(task);
   return (
     <>
-      <Table columns={task} dataSource={datas} scroll={{ x: 1300 }} />
+      <Table columns={columns} dataSource={datas} scroll={{ x: 1300 }} />
     </>
   );
 };
--- services/distributeStudents.ts
+++ services/distributeStudents.ts
@@ -11,50 +11,50 @@ export const distribute = (array: IWorkDone[], task: string) => {
   if (result.length > 4) {
     result.forEach((element, i: number) => {
       if (result.length - 1 === i) {
-        /*    updateObjectField('completed_tasks', element.id, {
-              reviewers: [...element.reviewers, ...objStudentsArray.slice(0, 4)],
-            }); */
+        updateObjectField('completed_tasks', element.id, {
+          reviewers: [...element.reviewers, ...objStudentsArray.slice(0, 4)],
+        });
         return (element.reviewers = [...element.reviewers, ...objStudentsArray.slice(0, 4)]);
       }
       if (result.length - 2 === i) {
-        /*    updateObjectField('completed_tasks', element.id, {
-              reviewers: [
-                ...element.reviewers,
-                ...[...objStudentsArray.slice(-1), ...objStudentsArray.slice(0, 3)],
-              ],
-            }); */
+        updateObjectField('completed_tasks', element.id, {
+          reviewers: [
+            ...element.reviewers,
+            ...[...objStudentsArray.slice(-1), ...objStudentsArray.slice(0, 3)],
+          ],
+        });
         return (element.reviewers = [
           ...element.reviewers,
           ...[...objStudentsArray.slice(-1), ...objStudentsArray.slice(0, 3)],
         ]);
       }
       if (result.length - 3 === i) {
-        /*    updateObjectField('completed_tasks', element.id, {
-              reviewers: [
-                ...element.reviewers,
-                ...[...objStudentsArray.slice(-2), ...objStudentsArray.slice(0, 2)],
-              ],
-            }); */
+        updateObjectField('completed_tasks', element.id, {
+          reviewers: [
+            ...element.reviewers,
+            ...[...objStudentsArray.slice(-2), ...objStudentsArray.slice(0, 2)],
+          ],
+        });
         return (element.reviewers = [
           ...element.reviewers,
           ...[...objStudentsArray.slice(-2), ...objStudentsArray.slice(0, 2)],
         ]);
       }
       if (result.length - 4 === i) {
-        /*    updateObjectField('completed_tasks', element.id, {
-              reviewers: [
-                ...element.reviewers,
-                ...[...objStudentsArray.slice(-3), ...objStudentsArray.slice(0, 1)],
-              ],
-            }); */
+        updateObjectField('completed_tasks', element.id, {
+          reviewers: [
+            ...element.reviewers,
+            ...[...objStudentsArray.slice(-3), ...objStudentsArray.slice(0, 1)],
+          ],
+        });
         return (element.reviewers = [
           ...element.reviewers,
           ...[...objStudentsArray.slice(-3), ...objStudentsArray.slice(0, 1)],
         ]);
       } else {
-        /*   updateObjectField('completed_tasks', element.id, {
-              reviewers: [...element.reviewers, ...objStudentsArray.slice(i + 1, i + 5)],
-            }); */
+        updateObjectField('completed_tasks', element.id, {
+          reviewers: [...element.reviewers, ...objStudentsArray.slice(i + 1, i + 5)],
+        });
         return (element.reviewers = [
           ...element.reviewers,
           ...objStudentsArray.slice(i + 1, i + 5),
