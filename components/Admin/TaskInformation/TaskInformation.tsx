161e8fadc0132513c96d8bd8962402c55b19564c
cup0ra <cup0ra@users.noreply.github.com>
2020-10-03 18:00:25 +0300

    fix: delete buttom

--- components/Admin/TaskInformation/TaskInformation.tsx
+++ components/Admin/TaskInformation/TaskInformation.tsx
@@ -42,7 +42,7 @@ const TaskInformation: React.FC<PropsTaskInformation> = ({
     setTaskStage(value);
   };
 
-  const distributeTest = () => {
+  /*   const distributeTest = () => {
     dataCompletedTask.forEach((e) => {
       e.reviewers.push(e.student);
       updateObjectField('completed_tasks', e.id, {
@@ -51,7 +51,7 @@ const TaskInformation: React.FC<PropsTaskInformation> = ({
     });
 
     console.log(dataCompletedTask);
-  };
+  }; */
   return (
     <>
       <Card style={{ marginTop: 30 }}>
@@ -85,13 +85,13 @@ const TaskInformation: React.FC<PropsTaskInformation> = ({
             style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
           >
             <Space align="end" direction="vertical">
-              <Button
+              {/*      <Button
                 style={{ marginBottom: 20, width: 182 }}
                 type="primary"
                 onClick={distributeTest}
               >
                 Distribute TEST
-              </Button>
+              </Button> */}
               <Button
                 disabled={taskStage !== 'CROSS_CHECK'}
                 style={{ marginBottom: 20, width: 182 }}
